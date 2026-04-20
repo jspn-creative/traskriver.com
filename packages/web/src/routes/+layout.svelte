6367783d882f130c412a0abb4490edbc94bddaad
Josh Spinney <josh@jaspin.io>
2026-04-20 04:26:35 -0700

    chore: remove counterscale-proxy package and update dependencies (#4)

--- packages/web/src/routes/+layout.svelte
+++ packages/web/src/routes/+layout.svelte
@@ -1,25 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import * as Counterscale from '@counterscale/tracker';
 
 	let { children } = $props();
-
-	$effect(() => {
-		const hostname = window.location.hostname;
-		const isProduction = hostname === 'traskriver.com' || hostname === 'www.traskriver.com';
-
-		if (!isProduction) return;
-
-		Counterscale.init({
-			siteId: 'traskriver.com',
-			reporterUrl: 'https://counterscale-proxy.jspn.workers.dev'
-		});
-
-		return () => {
-			Counterscale.cleanup();
-		};
-	});
 </script>
 
 <svelte:head>
