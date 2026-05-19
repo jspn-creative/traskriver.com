62e0e63fa2072e84638b53cc6d847b4d82f56639
Josh Spinney <josh@jaspin.io>
2026-05-19 13:36:33 -0700

    chore: add Google tag

--- packages/web/src/routes/+layout.svelte
+++ packages/web/src/routes/+layout.svelte
@@ -8,6 +8,16 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<title>Trask River Cam</title>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-T527K8JSHJ"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-T527K8JSHJ');
+	</script>
 </svelte:head>
 
 {@render children()}
